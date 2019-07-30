6b4e40db2b66ff2377fa2f7da9d708c7e01e27be
zhangjiangqige <zhangjiangqige@gmail.com>
2019-07-30 10:37:42 -0400

    partial

---
+++ src/extension.ts
@@ -0,0 +1,78 @@
+/* --------------------------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ * ------------------------------------------------------------------------------------------ */
+
+import * as fs from "fs"
+import * as path from 'path';
+
+import { workspace, ExtensionContext } from 'vscode';
+import { LanguageClient, LanguageClientOptions, ServerOptions } from 'vscode-languageclient';
+
+export function activate(context: ExtensionContext) {
+    let serverOptions: ServerOptions = {
+        command: findJavaExecutable('java'),
+        args: [],
+        options: { cwd: workspace.workspaceFolders[0].uri.toString() }
+    }
+
+    // Options to control the language client
+    let clientOptions: LanguageClientOptions = {
+        // Register the server for java documents
+        documentSelector: ['java'],
+        synchronize: {
+            // Synchronize the setting section 'languageServerExample' to the server
+            configurationSection: 'checker-framework',
+            // Notify the server about file changes to '.java' files contain in the workspace
+            fileEvents: workspace.createFileSystemWatcher('**/*.java')
+        }
+    }
+
+    // Create the language client and start the client.
+    let disposable = new LanguageClient('checker-framework', 'Checker Framework', serverOptions, clientOptions).start();
+
+    // Push the disposable to the context's subscriptions so that the
+    // client can be deactivated on extension deactivation
+    context.subscriptions.push(disposable);
+}
+
+function serverArgs() {
+    return [];
+}
+
+// MIT Licensed code from: https://github.com/georgewfraser/vscode-javac
+function findJavaExecutable(binname: string) {
+    binname = correctBinname(binname);
+
+    // First search each JAVA_HOME bin folder
+    if (process.env['JAVA_HOME']) {
+        let workspaces = process.env['JAVA_HOME'].split(path.delimiter);
+        for (let i = 0; i < workspaces.length; i++) {
+            let binpath = path.join(workspaces[i], 'bin', binname);
+            if (fs.existsSync(binpath)) {
+                return binpath;
+            }
+        }
+    }
+
+    // Then search PATH parts
+    if (process.env['PATH']) {
+        let pathparts = process.env['PATH'].split(path.delimiter);
+        for (let i = 0; i < pathparts.length; i++) {
+            let binpath = path.join(pathparts[i], binname);
+            if (fs.existsSync(binpath)) {
+                return binpath;
+            }
+        }
+    }
+
+    // Else return the binary name directly (this will likely always fail downstream)
+    return null;
+}
+
+function correctBinname(binname: string) {
+    if (process.platform === 'win32')
+        return binname + '.exe';
+    else
+        return binname;
+}
