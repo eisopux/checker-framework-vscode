3163a1fae7e9d0dddd967485a132422dbc118172
Evette Chan-Lee <emchanle@uwaterloo.ca>
2021-05-25 19:46:26 -0400

    add prettier and husky hook for automatic formatting

--- src/strings.ts
+++ src/strings.ts
@@ -1,14 +1,15 @@
 export class Misc {
-    public static pluginID: string = 'checker-framework';
-    public static pluginName: string = 'Checker Framework';
-    public static checkerRelPath: string = 'checker/dist/checker.jar';
-    public static serverMainClass: string = 'org.checkerframework.languageserver.ServerMain';
-    public static optLanguageServerPath: string = 'languageServerPath';
-    public static optFrameworkpath: string = 'frameworkPath';
-    public static optCheckers: string = 'checkers';
-    public static optCommandLineOptions: string = 'commandLineOptions';
-    public static optCFOrg: string = 'checkerframework_org';
-    public static optCFRepo: string = 'checkerframework_repo';
-    public static optLSOrg: string = 'languageserver_org';
-    public static optLSRepo: string = 'languageserver_repo';
+  public static pluginID: string = "checker-framework";
+  public static pluginName: string = "Checker Framework";
+  public static checkerRelPath: string = "checker/dist/checker.jar";
+  public static serverMainClass: string =
+    "org.checkerframework.languageserver.ServerMain";
+  public static optLanguageServerPath: string = "languageServerPath";
+  public static optFrameworkpath: string = "frameworkPath";
+  public static optCheckers: string = "checkers";
+  public static optCommandLineOptions: string = "commandLineOptions";
+  public static optCFOrg: string = "checkerframework_org";
+  public static optCFRepo: string = "checkerframework_repo";
+  public static optLSOrg: string = "languageserver_org";
+  public static optLSRepo: string = "languageserver_repo";
 }
