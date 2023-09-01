09e030afa01d7763ca38baca7761c72e7096a48c
Aosen Xiong <82676488+Ao-senXiong@users.noreply.github.com>
2023-09-01 19:08:23 -0400

    Add redirect to downloading script (#179)

--- scripts/get-downloader-release.js
+++ scripts/get-downloader-release.js
@@ -1,10 +1,27 @@
 const https = require('https');
 const fs = require('fs');
-const url = require('url');
 
 const REPO = "eisopux/checker-framework-languageserver-downloader";
 const API_URL = `https://api.github.com/repos/${REPO}/releases/latest`;
 const JAR = "./checker-framework-languageserver-downloader.jar";
+
+function downloadFile(url, destination, callback) {
+    https.get(url, (response) => { 
+        // 302 is the HTTP status code for a temporary redirect. 
+        if (response.statusCode === 302) {
+            downloadFile(response.headers.location, destination, callback);
+            return;
+        }
+        
+        const file = fs.createWriteStream(destination);
+        response.pipe(file);
+        file.on('finish', () => {
+            file.close(callback);
+        });
+    }).on('error', (error) => {
+        console.error(`Got error during file download: ${error.message}`);
+    });
+}
 
 https.get(API_URL, {
     headers: {
@@ -12,23 +29,14 @@ https.get(API_URL, {
     }
 }, (response) => {
     let data = '';
-
     response.on('data', (chunk) => {
         data += chunk;
     });
-
     response.on('end', () => {
         const jsonResponse = JSON.parse(data);
         const downloadUrl = jsonResponse.assets[0].browser_download_url;
-
-        const file = fs.createWriteStream(JAR);
-        https.get(new URL(downloadUrl), (response) => {
-            response.pipe(file);
-            file.on('finish', () => {
-                file.close(() => {
-                    console.log(`Downloaded the latest release to ${JAR}.`);
-                });
-            });
+        downloadFile(downloadUrl, JAR, () => {
+            console.log(`Downloaded the latest release to ${JAR}.`);
         });
     });
 }).on('error', (error) => {
